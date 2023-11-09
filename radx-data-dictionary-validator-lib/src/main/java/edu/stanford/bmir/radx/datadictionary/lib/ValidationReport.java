1237c5aa9e834a801a2e3614f9791edc25046623
Matthew Horridge <matthew.horridge@stanford.edu>
2023-11-09 10:49:45 -0800

    Added Javadoc for ValidationReport

--- radx-data-dictionary-validator-lib/src/main/java/edu/stanford/bmir/radx/datadictionary/lib/ValidationReport.java
+++ radx-data-dictionary-validator-lib/src/main/java/edu/stanford/bmir/radx/datadictionary/lib/ValidationReport.java
@@ -3,9 +3,16 @@ package edu.stanford.bmir.radx.datadictionary.lib;
 import java.util.List;
 
 /**
+ *
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 2023-01-31
+ * <p>
+ * The `ValidationReport` class represents the result of validating a RADx CSV data dictionary.
+ * It contains information about the original CSV data, as well as a list of validation results.
+ *
+ * @param csv The CSV data dictionary that was validated.
+ * @param results A list of {@link ValidationResult} instances representing the validation results.
  */
 public record ValidationReport(Csv csv, List<ValidationResult> results) {
 
